c1dbe005a06ffb6fe079c82fc854474d7cd3f1ca
Robert-Urmanic <91885141+Robert-Urmanic@users.noreply.github.com>
2021-10-05 16:21:19 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+package com.engeto.urm.cviceni01;
+
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    String clientName = "";
+        String clientSurname = "";
+        String clientFullName = clientName + clientSurname;
+        int numberOfPurchases;
+        String companyCity;
+        double vehicleConsumptionPer100km;
+        double utpCableLength;
+        LocalDate clientDateOfBirth;
+        String SPZ;
+        long ipAddress;
+    }
+}
